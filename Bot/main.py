2f908de3f317706cad3e020a1a8f61a140bdd618
MatthieuThib <matthieu.thibaut@edu.devinci.fr>
2022-03-27 11:31:05 +0200

    add reactions to messages

--- Bot/main.py
+++ Bot/main.py
@@ -22,19 +22,19 @@ DEFAULTS_INTENTS = discord.Intents.default()
 DEFAULTS_INTENTS.members = True 
 PREFIX_COMMAND = "$" 
      
-# Instancing the bot and the spotify client
+# Instancing discord bot and the spotify client
 bot = commands.Bot(command_prefix = PREFIX_COMMAND, intents = DEFAULTS_INTENTS)
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 @bot.event
 async def on_ready():
     print(" Spotib0t ready 🎵 ")
-    
+       
 @bot.event
 async def on_message(message):
     print("> ", message.content)
     # Ignore bot messages to prevent self-response
-    if(message.author.bot == False and not message.content.lower().startswith(('$'))): 
+    if(message.author.bot == False and not message.content.lower().startswith(('$'))):
 
         # Check if the message contains vulgarity
         if check_vulgarity(message):
@@ -43,17 +43,21 @@ async def on_message(message):
 
         # Check and respond to greetings
         elif message.content.lower().startswith(('hi','hello','good morning', 'good evening', 'hey','hola', 'yo', 'wesh')):
+            await message.add_reaction("👋")
             await message.channel.send("Hey **" + message.author.name + " !** I'm **Spotibot** 😎. \nI'm your new best friend (yes I assure you 🐣).\nI love music on Spotify ! I know everything 🎧 \nWhat can I do for you ? 😁")
 
         # Check and respond to goodbye-like messages
         elif(re.search("(B|b)ye|(B|b)ye bye|(H|h)ave a nice day|(S|s)ayonara|(A|a)u revoir|(B|b)onne journée|(E|e)xit|(K|k)ill", message.content)):
+            await message.add_reaction("👋")
             await message.channel.send("Bye **" + message.author.name + " !** \n See you soon 🐣") 
     
         # Analyse message and send a response
-        else: 
+        else:
+            await message.add_reaction("🎵")
+            await message.channel.send(" Working on it ... 🤔", delete_after = 5)
             response = get_response(spotify, message.content)
             await message.channel.send(response)
-       
+                   
     # Run bot commands if needed
     await bot.process_commands(message)
         
@@ -63,28 +67,24 @@ def check_vulgarity(message) :
     vulgarity = [True if element in ['fuck','bitch','fucking','motherfucker','shit', 'shut up', 'bastard', 'jerk'] else False for element in words]
     return True in vulgarity 
             
-            
-            
 @bot.command(name = "join", aliases = ["j"]) 
-async def join():
-    bot.login(SPOTIB0T_TOKEN)
+async def join(context):
+    await context.send(" Hi there ! I'm **Spotibot** 😎. \nI'm your new best friend (yes I assure you 🐣).\nI love music on Spotify ! I know everything 🎧 \nWhat can I do for you ? 😁")
     
-@bot.command(name='quit', aliases=['q'])
-async def quit():
-    bot.logout()
-    
+@bot.command(name = 'quit', aliases = ['q'])
+async def quit(context):
+    await context.channel.send(" Bye bye 👋")
             
-@bot.command(name='delete', aliases=['del', 'd'])
+@bot.command(name = 'delete', aliases = ['del', 'd'])
 async def delete(context, number: int):
     messages = await context.channel.history(limit=number + 1).flatten()
     for message in messages:
         await message.delete()
          
-@bot.command(name = "gif", aliases=['GIF','Gif'])
+@bot.command(name = "gif", aliases = ['g','Gif'])
 async def gif(context, name: str):
     gf = get_gifs_url(name, API_GIPHY_TOKEN)
     await context.channel.send(gf)
-    
     
 # Run Spotib0t
 bot.run(SPOTIB0T_TOKEN)
